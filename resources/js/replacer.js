681dec2d7d4a21553b3420f878f4e53c95a587bf
Kristof Feys <kristof.feys@me.com>
2020-05-19 09:33:16 +0200

    Only show distinct keys in translator

--- resources/js/replacer.js
+++ resources/js/replacer.js
@@ -23,5 +23,12 @@ export default function() {
         });
     }
 
-    return translationStrings;
+    let uniqueTranslationStrings = [];
+    for(let i = 0; i < translationStrings.length; i++) {
+        if(!uniqueTranslationStrings.map(v => v.key).includes(translationStrings[i].key)) {
+            uniqueTranslationStrings.push(translationStrings[i]);
+        }
+    }
+
+    return uniqueTranslationStrings;
 };
