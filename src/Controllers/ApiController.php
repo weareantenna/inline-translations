693a0fd12b35b93434de8464d1e38d35c863e845
Kristof Feys <kristof.feys@me.com>
2020-06-08 14:14:46 +0200

    Add translations updated event

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Antenna\InlineTranslations\Controllers;
 
+use Antenna\InlineTranslations\Events\TranslationUpdated;
+use Antenna\InlineTranslations\Models\TranslationKey;
 use Antenna\InlineTranslations\Requests\TranslationRequest;
 use Antenna\InlineTranslations\TranslationFetcher;
 use Antenna\InlineTranslations\TranslationUpdater;
@@ -26,6 +28,7 @@ class ApiController extends BaseController
     public function upsert(TranslationRequest $request, TranslationUpdater $updater) : JsonResponse
     {
         $result = $updater->updateTranslation($request->key, $request->value, $request->language);
+        event(new TranslationUpdated(TranslationKey::fromString($request->key), $request->value, $request->language));
 
         return new JsonResponse(['result' => $result]);
     }
---
+++ src/Events/TranslationUpdated.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Antenna\InlineTranslations\Events;
+
+use Antenna\InlineTranslations\Models\TranslationKey;
+
+class TranslationUpdated
+{
+    public TranslationKey $key;
+    public string $newValue;
+    public string $locale;
+
+    public function __construct(TranslationKey $key, $newValue, $locale)
+    {
+        $this->key = $key;
+        $this->newValue = $newValue;
+        $this->locale = $locale;
+    }
+}
